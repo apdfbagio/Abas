f944bd6cb839a376095daf1d6deb72c660c6c81b
Franco Bagio <apdfbagio@gmail.com>
2023-03-17 08:11:51 -0300

    First load

---
+++ app/src/main/java/com/adfbagio/abas/MainActivity.java
@@ -0,0 +1,40 @@
+package com.adfbagio.abas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.adfbagio.abas.Fragment.EmAltaFragment;
+import com.adfbagio.abas.Fragment.HomeFragment;
+import com.adfbagio.abas.Fragment.IncricoesFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        smartTabLayout=findViewById(R.id.viewPagerTab);
+        viewPager=findViewById(R.id.viewPager);
+//conf adapter p as abas
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Home", HomeFragment.class)
+                        .add("Inscriçoes", IncricoesFragment.class)
+                        .add("Em Alta", EmAltaFragment.class)
+                        .create());
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+    }
+}
